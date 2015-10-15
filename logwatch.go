b5989f15a6e745b4affe731da6d42692cc80296b
pekiZG <petar.slat@gmail.com>
2015-10-15 17:06:09 +0200

    Pushing "helloWorld" message to Slack finaly working
    Prototype working, make a wrapper function.

--- logwatch.go
+++ logwatch.go
@@ -3,23 +3,15 @@ package main
 import (
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 func main() {
-	slackAPI := "" // Put your incoming webHookURL here!
-
-	client := http.Client{}
-
-	form := url.Values{"text": {"This is a line of text in a channel."}}
-
-	req, err := http.NewRequest("POST", slackAPI, strings.NewReader(form.Encode()))
-
-	resp, err := client.Do(req)
+	slackIncomingWebhookAPI := "" // Put your incoming webHookURL here!
+	message := `{"text": "Hello World from GoLang!"}`
+	resp, err := http.PostForm(slackIncomingWebhookAPI, url.Values{"payload": {message}})
 
 	if err != nil {
 		// handle error here, but I'm just lazy
 	}
 	defer resp.Body.Close()
-
 }
